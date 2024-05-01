8a07360723160d02e0c7f54286015130aea69368
Malik <boutanmalik@gmail.com>
2024-05-01 00:30:20 -0500

    Commit 1

---
+++ src/components/Home.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react'
+import './Home.css';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="OverallContainer">
+
+            <div className="First">
+                <div className="FirstLeft">
+
+
+
+
+                </div>
+
+
+                <div className="FirstRight">
+
+                <div className= "inFR">
+                <h1 className="PrS">Project Smootheware</h1>
+                <div className="btxt2">
+
+                <h3 className="txt2">Your one-stop </h3>
+                <h3 className="txt2">software </h3>
+                <h3 className="txt2">center </h3>
+
+                </div>
+
+                </div>
+
+
+                
+
+
+
+
+
+
+                </div>
+
+            </div>
+
+
+
+
+            <div className="Second">
+                <div className="SecondLeft">
+                <div className= "inFR">
+                <h1 className="PrS2">Find tools</h1>
+                <div className="btxt3">
+
+                <h3 className="txt2">to enchance your </h3>
+                <h3 className="txt2"> productivity </h3>
+
+                </div>
+
+                </div>
+                    
+                </div>
+                <div className="SecondRight">
+
+                </div>
+
+
+
+
+
+
+
+
+            </div>
+
+            <div className="Third">
+                <div className="ThirdLeft">
+
+
+
+
+                </div>
+
+
+                <div className="ThirdRight">
+
+                <div className= "inFR">
+                <h1 className="PrS3">Find everything</h1>
+                <div className="btxt4">
+
+                <h3 className="txt2">From tools </h3>
+                <h3 className="txt2">to software</h3>
+                <h3 className="txt2">to games! </h3>
+
+                </div>
+
+                </div>
+
+
+                
+
+
+
+
+
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+            </div>
+            
+        )
+    }
+}
